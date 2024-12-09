aeca76fb45e150ec1821561ad3e19ee98ff4f265
BT_4N2_25 <sample@bths.edu>
2024-12-09 13:18:32 -0500

    Closer but needs more work. not sure where

--- src/Day2.java
+++ src/Day2.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day2 {
@@ -19,6 +18,8 @@ public class Day2 {
 
             int first = original.getFirst();
             int next = original.get(1);
+            int firstCheck;
+            int nextCheck;
             boolean increasing = true;
             boolean failChange = false;
 
@@ -33,19 +34,94 @@ public class Day2 {
 
                 if (increasing) {
                     if (first > next || (next - first) > 3 || first == next) {
-                        ArrayList<Integer> option1;
-                        option1 = new ArrayList<>(original.subList(0, i));
-                        //add the rest of the list to option 1. Do the same for option 2.
-                        //then check which one works. Whichever does stays
-                        //If none, you could end early with break and add invalid
+                        ArrayList<Integer> option1 = new ArrayList<>(original.subList(0, i));
+                        ArrayList<Integer> option1rest = new ArrayList<>(original.subList(i + 1, original.size()));
+                        option1.addAll(option1rest);
+                        boolean option1Valid = true;
+                        boolean option2Valid = true;
+
                         System.out.println("option 1: " + option1);
-                        original.remove(i);
+                        ArrayList<Integer> option2 = new ArrayList<>(original.subList(0, i + 1));
+                        ArrayList<Integer> option2rest = new ArrayList<>(original.subList(i + 2, original.size()));
+                        option2.addAll(option2rest);
+                        System.out.println("option 2: " + option2);
+
+                        for (int j = 0; j < option1.size() - 1; j++) {
+                            firstCheck = option1.get(j);
+                            nextCheck = option1.get(j + 1);
+                            if (firstCheck > nextCheck || (nextCheck - firstCheck) > 3 || firstCheck == nextCheck) {
+                                option1Valid = false;
+                                break;
+                            }
+                        }
+
+                        if (!option1Valid) {
+                            for (int j = 0; j < option2.size() - 1; j++) {
+                                firstCheck = option2.get(j);
+                                nextCheck = option2.get(j + 1);
+                                if (firstCheck > nextCheck || (nextCheck - firstCheck) > 3 || firstCheck == nextCheck) {
+                                    option2Valid = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!option1Valid && !option2Valid) {
+                            invalid++;
+                            break;
+                        }
+
+                        if (option1Valid) {
+                            original = option1;
+                        } else {
+                            original = option2;
+                        }
                         i--;
                     }
                 } else {
                     if (first < next || (first - next) > 3 || first == next) {
-                        original.remove(i);
-                        i--;
+                        ArrayList<Integer> option1 = new ArrayList<>(original.subList(0, i));
+                        ArrayList<Integer> option1rest = new ArrayList<>(original.subList(i + 1, original.size()));
+                        option1.addAll(option1rest);
+                        boolean option1Valid = true;
+                        boolean option2Valid = true;
+
+                        System.out.println("option 1: " + option1);
+                        ArrayList<Integer> option2 = new ArrayList<>(original.subList(0, i + 1));
+                        ArrayList<Integer> option2rest = new ArrayList<>(original.subList(i + 2, original.size()));
+                        option2.addAll(option2rest);
+                        System.out.println("option 2: " + option2);
+
+                        for (int j = 0; j < option1.size() - 1; j++) {
+                            firstCheck = option1.get(j);
+                            nextCheck = option1.get(j + 1);
+                            if (firstCheck < nextCheck || (firstCheck- nextCheck) > 3 || firstCheck == nextCheck) {
+                                option1Valid = true;
+                                original = option1;
+                            }
+                        }
+
+                        if (!option1Valid) {
+                            for (int j = 0; j < option2.size() - 1; j++) {
+                                firstCheck = option2.get(j);
+                                nextCheck = option2.get(j + 1);
+                                if (firstCheck < nextCheck || (firstCheck- nextCheck) > 3 || firstCheck == nextCheck) {
+                                    option2Valid = true;
+                                    original = option2;
+                                }
+                            }
+                        }
+
+                        if (!option1Valid && !option2Valid) {
+                            invalid++;
+                            break;
+                        }
+
+                        if (option1Valid) {
+                            original = option1;
+                        } else {
+                            original = option2;
+                        }
                     }
                 }
             }
